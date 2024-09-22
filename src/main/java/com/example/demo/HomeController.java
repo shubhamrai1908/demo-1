9ab43e7f24b7bb928f79c7fb7fe2acb04584947e
shubham rai <shubhamrai1908@gmail.com>
2024-09-22 18:01:14 +0530

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@GetMapping("/api")
+	@ResponseBody()
+	public String fun() {
+		return "HELLO WORLD!";
+	}
+
+}
