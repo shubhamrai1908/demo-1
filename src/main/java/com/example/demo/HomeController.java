44ed54df4fb04b1aec6a605f697ca5a0bc77cb04
shubham rai <shubhamrai1908@gmail.com>
2024-09-22 19:29:56 +0530

    cross origin

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+	@CrossOrigin(origins = "https://react-app-production-441e.up.railway.app")
 	@GetMapping("/api")
 	@ResponseBody()
 	public String fun() {
